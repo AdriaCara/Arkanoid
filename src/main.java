5c5b4e2f061e7142291255aedcb5ac9390770148
AdriaCara <adriaProgrammer@gmail.com>
2022-05-03 15:38:53 +0200

    principal created.

--- src/main.java
+++ src/main.java
@@ -3,7 +3,16 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		main M = new main();
+		M.principal();
 
+	}
+	
+	public void principal() {
+		
+		
+		
 	}
 
 }
