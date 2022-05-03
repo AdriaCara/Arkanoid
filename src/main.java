74a6c1351fce886a4bf7124d8d4d77972ad2bbcb
AdriaCara <adriaProgrammer@gmail.com>
2022-05-03 16:45:49 +0200

    Created the ball and check the borders.

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		main M = new main();
@@ -10,12 +10,22 @@ public class main {
 
 	}
 	
-	public void principal() {
+	public void principal() throws InterruptedException {
 		
 		JFrame frame = new JFrame("Arkanoid");
+		Game game = new Game();
+		frame.add(game);
 		frame.setSize(600, 600);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		while (true) {
+			
+			game.moveBall();
+			game.repaint();
+			Thread.sleep(5);
+			
+		}
 		
 	}
 
