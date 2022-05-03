103a75a8c0c154964865da562cefd413007396c5
AdriaCara <adriaProgrammer@gmail.com>
2022-05-03 16:06:46 +0200

    Implements a little window that have for title Arkanoid.

---
+++ src/Brick.java
@@ -0,0 +1,20 @@
+
+public class Brick {
+	
+	private int width;
+	
+	private int PosHeight;
+	
+	private int lives;
+	
+	private int colaider;
+	
+	private Brick generateBrick(int type) {
+		
+		
+		
+		return null;
+		
+	}
+
+}
--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import javax.swing.JFrame;
 
 public class main {
 
@@ -11,7 +12,10 @@ public class main {
 	
 	public void principal() {
 		
-		
+		JFrame frame = new JFrame("Arkanoid");
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
 
